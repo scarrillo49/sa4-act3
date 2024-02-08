35e6f052f3ad717f0e84f0aca9a03aef7336b3e5
Samantha Carrillo <samanthascarrillo@gmail.com>
2024-02-08 00:42:12 -0600

    Implemented random number generation and error handling.

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,25 @@
-number = 10
+import random
+
+number = random.randint(1, 100)  
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
 
-if guess == number:
-   print("Congratulations! You guessed the right number.")
-else:
-   print(f"Sorry! The number was {number}.")
+while True:
+    guess = input("What number am I thinking of? ").lower()  
+
+    if guess == 'q':
+        print(f"The number was {number}.")
+        break
+
+    try:
+        guess = int(guess)  
+
+        if guess == number:
+            print("Congratulations! You guessed the right number.")
+            break
+        else:
+            print("Try again!")
+    except ValueError:
+        print("Invalid input. Please enter a number or 'q' to quit.")
+
+print("Thanks for playing!")
